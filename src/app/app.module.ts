5ce35ce7dd3c4f9c27bf914774f19c851e3edb0e
Kievina <56592501+Kievina@users.noreply.github.com>
2020-01-06 10:18:45 -0500

    currently not displaying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { RightSideBarComponent } from './components/hub/right-side-bar/right-sid
 import { NavbarComponent } from './components/hub/navbar/navbar.component';
 import { PollComponent } from './components/hub/poll/poll.component';
 import { NewChatComponent } from './components/hub/user-chats/new-chat/new-chat.component';
+import { TopRightComponent } from './components/hub/right-side-bar/top-right/top-right.component';
+import { BottomRightComponent } from './components/hub/right-side-bar/bottom-right/bottom-right.component';
+
 
 
 @NgModule({
@@ -30,6 +33,8 @@ import { NewChatComponent } from './components/hub/user-chats/new-chat/new-chat.
     NavbarComponent,
     PollComponent,
     NewChatComponent,
+    TopRightComponent,
+    BottomRightComponent,
   ],
   imports: [
     BrowserModule,
