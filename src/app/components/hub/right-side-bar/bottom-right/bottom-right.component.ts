22d7262ab009366606e379e4aa61bc497d68e7bd
Kievina <56592501+Kievina@users.noreply.github.com>
2020-01-06 16:38:17 -0500

    small changes

---
+++ src/app/components/hub/right-side-bar/bottom-right/bottom-right.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-right',
+  templateUrl: './bottom-right.component.html',
+  styleUrls: ['./bottom-right.component.css']
+})
+export class BottomRightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
