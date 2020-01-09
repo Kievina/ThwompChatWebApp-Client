48e6d2862fd2613723984786b0cbf6504a151260
Kievina <56592501+Kievina@users.noreply.github.com>
2020-01-09 18:46:47 -0500

    update poll service ts

--- src/app/services/poll.service.ts
+++ src/app/services/poll.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Poll } from '../models/poll.model';
+import { Option } from '../models/option.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PollService {
+  public poll: Poll;
+  public option: Option;
+  public optionsList: Array<Option>;
 
-  constructor() { }
+
+  constructor(
+    private http: HttpClient,
+    private userService: UserService) { }
+
+  createPoll(poll, chatId: number) {
+    let header = new HttpHeaders();
+    header = header.append('content-type', 'application/json');
+    return this.http.post(`http://${window.location.hostname}:8080/chat/${chatId}/polls`, poll, { headers:header})
+      .subscribe(response => console.log(response));
+  }
 }
